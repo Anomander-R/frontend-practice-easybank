a50b727df4348930faca1dfd7944785f17c2270c
Anomander Rake <90348779+Anomander-R@users.noreply.github.com>
2023-06-07 21:19:00 +0200

    Mobile Menu Completed

--- app/index.js
+++ app/index.js
@@ -3,11 +3,17 @@ console.log('Script is working');
 const btnHamburger = document.getElementById('btnHamburger');
 const header = document.getElementById('header');
 const overlay = document.getElementById('overlay');
-
+const body = document.body;
+let flip = true;
 
 function onClick(){
+    // flip=!flip;
+    // if (flip){
+    //     body.classList.add("noscroll")
+    // }
     overlay.classList.add("fade-out");
     header.classList.toggle("open");
+    body.classList.toggle("noscroll");
 }
 
 btnHamburger.addEventListener('click', onClick);
